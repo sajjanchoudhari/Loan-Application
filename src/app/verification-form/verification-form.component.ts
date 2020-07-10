8cd78ad30afbd3a15016c359b5f74e604ee08957
sajjanchoudhari <sajjanchoudhari12@gmail.com>
2020-07-10 14:42:35 +0530

    Initial commit

---
+++ src/app/verification-form/verification-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verification-form',
+  templateUrl: './verification-form.component.html',
+  styleUrls: ['./verification-form.component.css']
+})
+export class VerificationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
